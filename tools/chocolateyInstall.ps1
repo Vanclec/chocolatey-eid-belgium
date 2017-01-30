08eb6d399ffb70f5ea2c8bff014190c290423570
Chris Vanclercq <chris@vanclercq.me>
2017-01-30 23:29:26 +0100

    Added checksum verification.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,6 +3,8 @@ $packageName			= 'eid-belgium';
 $toolsDir				= ($(Split-Path -Parent $MyInvocation.MyCommand.Definition) + '\');
 $url					= 'http://eid.belgium.be/sites/default/files/downloads/BeidMW_32_4.1.20.msi';
 $url64					= 'http://eid.belgium.be/sites/default/files/downloads/BeidMW_64_4.1.20.msi';
+$checkSum				= '2B93B7AA2ED5441854ACCA435FF9341A75AD4B5EA37778E011174D8568C612D3';
+$checkSum64				= '0A7ED9490FE11B7F105CEDE8970A387E8356A90413F2307F593E7714C9E26922';
 
 $packageArgs = @{
 	packageName		= $packageName
@@ -10,6 +12,10 @@ $packageArgs = @{
 	fileType		= 'MSI'
 	url				= $url
 	url64bit		= $url64
+	checkSum		= $checkSum
+	checkSum64		= $checkSum64
+	checksumType	= 'sha256'
+	checksumType64	= 'sha256'
 	silentArgs		= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 	validExitCodes	= @(0, 3010, 1641)
 }
