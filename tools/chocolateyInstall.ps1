07dd99ff9c24916cebbfc1089f4d3496b3e96a99
Chris Vanclercq <chris@vanclercq.me>
2017-05-28 21:42:16 +0200

    Mise à jour des URLs suite à la refonte du site officiel de l'eID.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference	= 'Stop';
 $packageName			= 'eid-belgium';
 $toolsDir				= ($(Split-Path -Parent $MyInvocation.MyCommand.Definition) + '\');
-$url					= 'http://eid.belgium.be/sites/default/files/downloads/BeidMW_32_4.1.20.msi';
-$url64					= 'http://eid.belgium.be/sites/default/files/downloads/BeidMW_64_4.1.20.msi';
+$url					= 'https://eid.belgium.be/sites/default/files/software/beidmw_32_4.1.20.msi';
+$url64					= 'https://eid.belgium.be/sites/default/files/software/beidmw_64_4.1.20.msi';
 $checkSum				= '2B93B7AA2ED5441854ACCA435FF9341A75AD4B5EA37778E011174D8568C612D3';
 $checkSum64				= '0A7ED9490FE11B7F105CEDE8970A387E8356A90413F2307F593E7714C9E26922';
 
